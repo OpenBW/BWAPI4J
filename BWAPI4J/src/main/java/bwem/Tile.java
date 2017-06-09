f481f8c89b30c0ba5de157fd2c3ba8b928fecf6b
Adakite Systems <adakitesystems@gmail.com>
2017-06-09 03:22:20 -0600

    Update "Tile.java" with missing "Neutral" functionality

--- BWAPI4J/src/main/java/bwem/Tile.java
+++ BWAPI4J/src/main/java/bwem/Tile.java
@@ -24,24 +24,23 @@ import org.apache.commons.lang3.mutable.MutableInt;
 //class Tile : public utils::Markable<Tile, int>, public utils::UserData
 public class Tile {
 
-//    Neutral *			m_pNeutral = nullptr; // TODO
+    private Neutral m_pNeutral;
     private Altitude m_minAltitude;
     private int m_areaId;
     MutableInt m_internalData;
     Bits m_bits;
 
     public Tile() {
-        m_minAltitude = new Altitude(0);
-        m_areaId = 0;
-        m_internalData = new MutableInt(0);
-        m_bits = new Bits();
+        this.m_minAltitude = new Altitude(0);
+        this.m_areaId = 0;
+        this.m_internalData = new MutableInt(0);
+        this.m_bits = new Bits();
     }
 
 	// Corresponds to BWAPI::isBuildable
 	// Note: BWEM enforces the relation buildable ==> walkable (Cf. MiniTile::Walkable)
-//	bool				Buildable() const				{ return m_bits.buildable; }
     public boolean Buildable() {
-        return (m_bits.buildable != 0x0);
+        return (this.m_bits.buildable != 0x0);
     }
 
     // Tile::AreaId() somewhat aggregates the MiniTile::AreaId() values of the 4 x 4 sub-MiniTiles.
@@ -50,18 +49,18 @@ public class Tile {
     // If S = {a}, returns a (whether positive or negative).
     // If size(S) > 1 returns -1 (note that -1 is never returned by MiniTile::AreaId()).
 	public int AreaId(){
-        return m_areaId;
+        return this.m_areaId;
     }
 
     // Tile::MinAltitude() somewhat aggregates the MiniTile::Altitude() values of the 4 x 4 sub-MiniTiles.
     // Returns the minimum value.
 	public Altitude MinAltitude() {
-        return new Altitude(m_minAltitude);
+        return new Altitude(this.m_minAltitude);
     }
 
     // Tells if at least one of the sub-MiniTiles is Walkable.
 	public boolean Walkable() {
-        return (m_areaId != 0);
+        return (this.m_areaId != 0);
     }
 
     // Tells if at least one of the sub-MiniTiles is a Terrain-MiniTile.
@@ -72,13 +71,13 @@ public class Tile {
     // 0: lower ground    1: high ground    2: very high ground
     // Corresponds to BWAPI::getGroundHeight / 2
     public int GroundHeight() {
-        return ((int) m_bits.groundHeight);
+        return ((int) this.m_bits.groundHeight);
     }
 
     // Tells if this Tile is part of a doodad.
     // Corresponds to BWAPI::getGroundHeight % 2
     public boolean Doodad() {
-        return (m_bits.doodad != 0x0);
+        return (this.m_bits.doodad != 0x0);
     }
 
     // TODO
@@ -91,7 +90,9 @@ public class Tile {
     // is destroyed and BWEM is informed of that by a call of Map::OnMineralDestroyed(BWAPI::Unit u) for exemple. In such a case,
     // BWEM automatically updates the data by deleting the Neutral instance and clearing any reference to it such as the one
     // returned by Tile::GetNeutral(). In case of stacked Neutrals, the next one is then returned.
-//	Neutral *			GetNeutral() const				{ return m_pNeutral; }
+	public Neutral GetNeutral() {
+        return this.m_pNeutral;
+    }
 
     // TODO
 	// Returns the number of Neutrals that occupy this Tile (Cf. GetNeutral).
@@ -101,7 +102,7 @@ public class Tile {
     //	Details: The functions below are used by the BWEM's internals
 
     public void SetBuildable() {
-        m_bits.buildable = 0x1;
+        this.m_bits.buildable = 0x1;
     }
 
     public void SetGroundHeight(int h) {
@@ -109,30 +110,31 @@ public class Tile {
         if (!((h >= 0) && (h <= 2))) {
             throw new IllegalArgumentException("h=" + h);
         }
-        m_bits.groundHeight = (byte) h;
+        this.m_bits.groundHeight = (byte) h;
     }
 
     public void SetDoodad() {
-        m_bits.doodad = 0x1;
+        this.m_bits.doodad = 0x1;
     }
 
-    // TODO
-//    public void AddNeutral(Neutral pNeutral) {
-//        bwem_assert(!m_pNeutral && pNeutral);
-//        m_pNeutral = pNeutral;
-//    }
-
-    public void SetAreaId(int id) {
-//        bwem_assert((id == -1) || !m_areaId && id);
-//        assert ((id == -1) || (m_areaId == 0 && id != 0)); /* Assertions shouldn't be used in public methods as validation even though these methods are used by BWEM's internals. */
-        if (!((id == -1) || (m_areaId == 0 && id != 0))) {
+    public void AddNeutral(Neutral pNeutral) {
+//        bwem_assert(!m_pNeutral && pNeutral); /* Assertions shouldn't be used in public methods as validation even though these methods are used by BWEM's internals. */
+        if (!(m_pNeutral == null && pNeutral != null)) {
             throw new IllegalStateException();
         }
-        m_areaId = id;
+        this.m_pNeutral = pNeutral;
+    }
+
+    public void SetAreaId(int id) {
+//        bwem_assert((id == -1) || !m_areaId && id); /* Assertions shouldn't be used in public methods as validation even though these methods are used by BWEM's internals. */
+        if (!((id == -1) || (this.m_areaId == 0 && id != 0))) {
+            throw new IllegalStateException();
+        }
+        this.m_areaId = id;
     }
 
     public void ResetAreaId() {
-        m_areaId = 0;
+        this.m_areaId = 0;
     }
 
     public void SetMinAltitude(Altitude altitude) {
@@ -140,22 +142,24 @@ public class Tile {
         if (!(altitude.toInt() >= 0)) {
             throw new IllegalArgumentException("altitude=" + altitude.toInt());
         }
-        m_minAltitude = new Altitude(altitude);
+        this.m_minAltitude = new Altitude(altitude);
     }
 
-    // TODO
-//    public void RemoveNeutral(Neutral * pNeutral) {
-//        bwem_assert(pNeutral && (m_pNeutral == pNeutral));
+    public void RemoveNeutral(Neutral pNeutral) {
+//        bwem_assert(pNeutral && (m_pNeutral == pNeutral)); /* Assertions shouldn't be used in public methods as validation even though these methods are used by BWEM's internals. */
+        if (!(pNeutral != null && m_pNeutral == pNeutral)) {
+            throw new IllegalStateException();
+        }
 //        utils::unused(pNeutral);
-//        m_pNeutral = nullptr;
-//    }
+        this.m_pNeutral = null;
+    }
 
     public int InternalData() {
-        return m_internalData.intValue();
+        return this.m_internalData.intValue();
     }
 
     public void SetInternalData(int data) {
-        m_internalData.setValue(data);
+        this.m_internalData.setValue(data);
     }
 
 }
