983b97d348885dcf534642193822c70b45301288
Adakite Systems <adakitesystems@gmail.com>
2017-06-13 04:37:11 -0600

    Update Neutral.java
    - Mostly complete.
    - Method "vector<const Area *> Neutral::BlockedAreas() const"
    depends on class "Area" which is not implemented yet.

--- BWAPI4J/src/main/java/bwem/Neutral.java
+++ BWAPI4J/src/main/java/bwem/Neutral.java
@@ -4,101 +4,103 @@ import java.util.List;
 import org.openbw.bwapi4j.Position;
 import org.openbw.bwapi4j.TilePosition;
 import org.openbw.bwapi4j.WalkPosition;
-import org.openbw.bwapi4j.type.UnitType;
 import org.openbw.bwapi4j.unit.Unit;
 
 public class Neutral {
 
-    private Unit m_bwapiUnit = null;
-    private UnitType m_bwapiType = null;
-    private Position m_pos = null;
-    private TilePosition m_topLeft = null;
-    private TilePosition m_size = null;
-    private Map m_pMap = null;
-    private Neutral m_pNextStacked = null;
-    private List<WalkPosition> m_blockedAreas = null;
+    private Unit unit = null;
+    private Position position = null;
+    private TilePosition topLeft = null;
+    private TilePosition size = null;
+    private Map map = null;
+    private Neutral nextStacked = null;
+    private List<WalkPosition> blockedAreas = null;
 
     private Neutral() {}
 
     protected Neutral(Unit unit, Map pMap) {
-        this.m_bwapiUnit = unit;
-        this.m_bwapiType = unit.getInitialType();
-        this.m_pMap = pMap;
-        this.m_pos = unit.getInitialPosition();
-        this.m_topLeft = unit.getInitialTilePosition();
+        this.unit = unit;
+        this.map = pMap;
+        this.position = unit.getPosition();
+        this.topLeft = this.position.toTilePosition();
+//        if (this.m_bwapiType == UnitType.Special_Right_Pit_Door) {
+//            this.position = this.position.add(new Position(1, 0));
+//        }
 
-        if (this.m_bwapiType == UnitType.Special_Right_Pit_Door) {
-            this.m_pos = this.m_pos.add(new Position(1, 0));
-        }
-
-        PutOnTiles(); //TODO
+        putOnTiles(); //TODO
     }
 
-    public Unit Unit() {
-        return this.m_bwapiUnit;
+    public Unit getUnit() {
+        return this.unit;
     }
 
-    public UnitType Type() {
-        return this.m_bwapiType;
+    public Position getPosition() {
+        return this.position;
     }
 
-    public Position Pos() {
-        return this.m_pos;
+    public TilePosition getTopLeft() {
+        return this.topLeft;
     }
 
-    public TilePosition TopLeft() {
-        return this.m_topLeft;
+    public TilePosition getBottomRight() {
+        return this.topLeft.add(this.size).subtract(new TilePosition(1, 1));
     }
 
-    public TilePosition BottomRight() {
-        return this.m_topLeft.add(m_size).subtract(new TilePosition(1, 1));
+    public TilePosition getSize() {
+        return this.size;
     }
 
-    public TilePosition Size() {
-        return this.m_size;
-    }
-
-    public boolean Blocking() {
-        return !this.m_blockedAreas.isEmpty();
-    }
-
-    public List<WalkPosition> BlockedAreas() {
-        return this.m_blockedAreas;
-    }
-
-    public Neutral NextStacked() {
-        return this.m_pNextStacked;
-    }
-
-//    Neutral LastStacked() { Neutral * pTop = this; while (pTop->m_pNextStacked) pTop = pTop->m_pNextStacked; return pTop; }
-    public Neutral LastStacked() {
-        Neutral pTop = NextStacked();
-        while (pTop != null) {
-            pTop = pTop.NextStacked();
-        }
-        return pTop;
-    }
-
-    public void SetBlocking(List<WalkPosition> blockedAreas) {
-        this.m_blockedAreas = blockedAreas;
-    }
-
-    protected Map GetMap() {
-        return this.m_pMap;
+    public boolean isBlocking() {
+        return !this.blockedAreas.isEmpty();
     }
 
     //TODO
-    private void PutOnTiles() {
-//        bwem_assert(!m_pNextStacked); //TODO: next stacked
+//    vector<const Area *> Neutral::BlockedAreas() const
+//    {
+//        vector<const Area *> Result;
+//        for (WalkPosition w : m_blockedAreas)
+//            Result.push_back(GetMap()->GetArea(w));
+//
+//        return Result;
+//    }
 
-        for (int dy = 0 ; dy < Size().getY() ; ++dy)
-        for (int dx = 0 ; dx < Size().getX() ; ++dx)
+    public Neutral getNextStacked() {
+        return this.nextStacked;
+    }
+
+    public Neutral getLastStacked() {
+        Neutral last = getNextStacked();
+        while (last != null) {
+            last = last.getNextStacked();
+        }
+        return last;
+    }
+
+    public void setBlockingAreas(List<WalkPosition> blockedAreas) {
+        this.blockedAreas = blockedAreas;
+    }
+
+    protected Map getMap() {
+        return this.map;
+    }
+
+    //TODO
+    private void putOnTiles() {
+//        bwem_assert(!m_pNextStacked);
+        if (this.nextStacked == null) {
+            throw new IllegalStateException();
+        }
+
+        for (int dy = 0 ; dy < getSize().getY() ; ++dy)
+        for (int dx = 0 ; dx < getSize().getX() ; ++dx)
         {
-            Tile tile = this.m_pMap.getTile(TopLeft().add(new TilePosition(dx, dy)));
-            if (tile.getNeutral() != null) tile.setNeutral(this);
-            else
-            {
-                Neutral pTop = tile.getNeutral().LastStacked();
+            Tile tile = this.map.getTile(getTopLeft().add(new TilePosition(dx, dy)));
+            if (tile.getNeutral() != null)  {
+                //TODO: What is this for or how does this work?
+                //neutral.cpp:74:if (!tile.GetNeutral()) tile.AddNeutral(this);
+                tile.setNeutral(this);
+            } else {
+                Neutral lastStacked = tile.getNeutral().getLastStacked();
                 //TODO
 //                bwem_assert(this != tile.GetNeutral());
 //                bwem_assert(this != pTop);
@@ -107,7 +109,7 @@ public class Neutral {
 //                bwem_assert_plus(pTop->TopLeft() == TopLeft(), "stacked neutrals not aligned: " + my_to_string(pTop->TopLeft()) + " / " + my_to_string(TopLeft()));
 //                bwem_assert((dx == 0) && (dy == 0));
 //
-                pTop.m_pNextStacked = this;
+                lastStacked.nextStacked = this;
                 return;
             }
         }
