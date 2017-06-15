59e8a98a60203e35ea7c3b3767980824291cc7e0
Adakite Systems <adakitesystems@gmail.com>
2017-06-15 00:45:23 -0600

    Delete

--- BWAPI4J/src/main/java/bwem/BWEM.java
+++ BWAPI4J/src/main/java/bwem/BWEM.java
@@ -1,44 +1,42 @@
 package bwem;
 
-import org.openbw.bwapi4j.WalkPosition;
-
 public class BWEM {
 
     public static final int MAX_LAKE_MINI_TILES = 300;
 
-    /* map.cpp:29 */
-//    bool seaSide(WalkPosition p, const Map * pMap)
-//    {
-//        if (!pMap->GetMiniTile(p).Sea()) return false;
+//    /* map.cpp:29 */
+////    bool seaSide(WalkPosition p, const Map * pMap)
+////    {
+////        if (!pMap->GetMiniTile(p).Sea()) return false;
+////
+////        for (WalkPosition delta : {WalkPosition(0, -1), WalkPosition(-1, 0), WalkPosition(+1, 0), WalkPosition(0, +1)})
+////            if (pMap->Valid(p + delta))
+////                if (!pMap->GetMiniTile(p + delta, check_t::no_check).Sea())
+////                    return true;
+////
+////        return false;
+////    }
+//    /**
+//     * Tests if the specified position has any non-sea neighbors.
+//     *
+//     * @param p specified position
+//     * @param map specified map containing the specified position
+//     */
+//    public static boolean hasNonSeaNeighbor(WalkPosition p, Map map) {
+//        if (!map.getMiniTile(p).isSea()) {
+//            return false;
+//        }
 //
-//        for (WalkPosition delta : {WalkPosition(0, -1), WalkPosition(-1, 0), WalkPosition(+1, 0), WalkPosition(0, +1)})
-//            if (pMap->Valid(p + delta))
-//                if (!pMap->GetMiniTile(p + delta, check_t::no_check).Sea())
-//                    return true;
+//        WalkPosition[] deltas = { new WalkPosition(0, -1), new WalkPosition(-1, 0), new WalkPosition(1, 0), new WalkPosition(0, 1) };
+//        for (WalkPosition delta : deltas) {
+//            WalkPosition pDelta = p.add(delta);
+//            if (map.isValid(pDelta) && !map.getMiniTile(pDelta).isSea()) {
+//                return true;
+//            }
+//        }
 //
 //        return false;
 //    }
-    /**
-     * Tests if the specified position has any non-sea neighbors.
-     *
-     * @param p specified position
-     * @param map specified map containing the specified position
-     */
-    public static boolean hasNonSeaNeighbor(WalkPosition p, Map map) {
-        if (!map.getMiniTile(p).isSea()) {
-            return false;
-        }
-
-        WalkPosition[] deltas = { new WalkPosition(0, -1), new WalkPosition(-1, 0), new WalkPosition(1, 0), new WalkPosition(0, 1) };
-        for (WalkPosition delta : deltas) {
-            WalkPosition pDelta = p.add(delta);
-            if (map.isValid(pDelta) && !map.getMiniTile(pDelta).isSea()) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     public static int squaredNorm(int dx, int dy) {
         return (dx * dx + dy * dy);
