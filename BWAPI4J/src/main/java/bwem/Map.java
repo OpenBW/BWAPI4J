de565fb199ff3c1d2f51ef7310972dfbbc566faa
Adakite Systems <adakitesystems@gmail.com>
2017-06-15 23:38:21 -0600

    Start porting "MapImpl::LoadData()"

--- BWAPI4J/src/main/java/bwem/Map.java
+++ BWAPI4J/src/main/java/bwem/Map.java
@@ -21,7 +21,9 @@ public class Map {
     private TilePosition tileSize = null;
     private WalkPosition walkSize = null;
     private Position pixelSize = null;
+    private Position center = null;
     private List<MiniTile> miniTiles = null;
+    private List<TilePosition> startLocations = null;
 
     private Map() {
         /* Do nothing. */
@@ -37,7 +39,59 @@ public class Map {
         this.tileSize = new TilePosition(this.bw.getBWMap().mapWidth(), this.bw.getBWMap().mapHeight());
         this.walkSize = new WalkPosition(getTileSize());
         this.pixelSize = getTileSize().toPosition();
+        this.center = new Position(getTileSize().getX() / 2, getTileSize().getY() / 2);
         this.miniTiles = new ArrayList<>();
+        this.startLocations = new ArrayList<>();
+        for (TilePosition t : this.bw.getBWMap().getStartPositions()) {
+            this.startLocations.add(t);
+        }
+
+        loadData(); //TODO
+    }
+
+    /**
+     * Computes walkability, buildability, groundHeight, and doodad information.
+     */
+    private void loadData() {
+        /* Mark unwalkable minitiles (minitiles are walkable by default). */
+        for (int y = 0; y < getWalkSize().getY(); ++y)
+        for (int x = 0; x < getWalkSize().getX(); ++x) {
+            if (!this.bw.getBWMap().isWalkable(x, y)) {
+                /**
+                 * For each unwalkable minitile, we also mark its eight neighbors as not walkable.
+                 * According to some tests, this prevents from wrongly pretending one Marine can go by some thin path.
+                 */
+                for (int dy = -1; dy <= 1; ++dy)
+                for (int dx = -1; dx <= 1; ++dx) {
+                    WalkPosition w = new WalkPosition(x + dx, y + dy);
+                    if (isValid(w)) {
+                        getMiniTile(w, CheckMode.NoCheck).setWalkable(false);
+                    }
+                }
+            }
+        }
+
+        /* Mark buildable tiles (tiles are unbuildable by default). */
+        for (int y = 0 ; y < getTileSize().getY() ; ++y)
+        for (int x = 0 ; x < getTileSize().getX() ; ++x) {
+            TilePosition t = new TilePosition(x, y);
+//            if (broodwar.getBWMap().isBuildable(t)) { //TODO: isBuildable is not implemented yet.
+            if (false) {
+//                getTile(t).setBuildable(); //TODO: getTile is not implemented yet.
+                /* Ensures buildable ==> walkable: */
+                for (int dy = 0 ; dy < 4 ; ++dy)
+                for (int dx = 0 ; dx < 4 ; ++dx) {
+                    getMiniTile(new WalkPosition(t).add(new WalkPosition(dx, dy)), CheckMode.NoCheck).setWalkable(true);
+                }
+            }
+
+            /* Set groundHeight and doodad information. */
+            int bwapiGroundHeight = bw.getBWMap().getGroundHeight(t);
+//            getTile(t).setGroundHeight(bwapiGroundHeight / 2); //TODO: getTile is not implemented yet.
+            if (bwapiGroundHeight % 2 != 0) {
+//                getTile(t).setDoodad(); //TODO: getTile is not implemented yet.
+            }
+        }
     }
 
     public TilePosition getTileSize() {
--- BWAPI4J/src/main/java/bwem/MiniTile.java
+++ BWAPI4J/src/main/java/bwem/MiniTile.java
@@ -4,10 +4,70 @@ public class MiniTile {
 
     public static final int SIZE_IN_PIXELS = 8;
 
-    private Area.Id id;
+    private Area.Id areaId = null;
+    private Altitude altitude = null;
+
+    public MiniTile() {
+        this.areaId = new Area.Id(-1);
+        this.altitude = new Altitude(-1);
+    }
 
     public Area.Id getAreaId() {
-        return new Area.Id(this.id);
+        return new Area.Id(this.areaId);
+    }
+
+    public void setAreaId(Area.Id areaId) {
+//        { bwem_assert(AreaIdMissing() && (id >= 1)); m_areaId = id; }
+        if (!isAreaIdMissing()) {
+            throw new IllegalStateException("Area.Id is already set");
+        } else if (!(areaId.intValue() >= 1)) {
+            throw new IllegalArgumentException("invalid Area.Id");
+        } else {
+            this.areaId = new Area.Id(areaId);
+        }
+    }
+
+    public void replaceAreaId(Area.Id areaId) {
+//        { bwem_assert((m_areaId > 0) && ((id >= 1) || (id <= -2)) && (id != m_areaId)); m_areaId = id; }
+        if (!((this.areaId.intValue() > 0)
+                && ((areaId.intValue() >= 1) || (areaId.intValue() <= -2))
+                && (areaId.intValue() != this.areaId.intValue()))) {
+            throw new IllegalArgumentException("invalid Area.Id");
+        } else {
+            this.areaId = new Area.Id(areaId);
+        }
+    }
+
+    public boolean isAreaIdMissing() {
+        return (this.areaId.intValue() == -1);
+    }
+
+    public Altitude getAltitude() {
+        return new Altitude(this.altitude);
+    }
+
+    public void setAltitude(Altitude altitude) {
+//        { bwem_assert_debug_only(AltitudeMissing() && (a > 0)); m_altitude = a; }
+        if (!isAltitudeMissing()) {
+            throw new IllegalStateException("Altitude is already set");
+        } else if (!(altitude.intValue() > 0)) {
+            throw new IllegalArgumentException("invalid Altitude");
+        } else {
+            this.altitude = new Altitude(altitude);
+        }
+    }
+
+    public boolean isAltitudeMissing() {
+        return (this.altitude.intValue() == -1);
+    }
+
+    public boolean isWalkable() {
+        return (this.areaId.intValue() != 0);
+    }
+
+    public void setWalkable(boolean walkable) {
+        this.areaId = new Area.Id(walkable ? -1 : 0);
+        this.altitude = new Altitude(walkable ? -1 : 1);
     }
 
 }
