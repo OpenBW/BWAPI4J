960dbf1581c90b6d13a8eb97e98cd0cbb9a4e2f8
IMP <imp@openbw.com>
2017-10-19 10:54:15 -0500

    small fix to properly detect Windows vs Linux (non-Windows) OS.

--- BWAPI4J/src/main/java/org/openbw/bwapi4j/BW.java
+++ BWAPI4J/src/main/java/org/openbw/bwapi4j/BW.java
@@ -59,7 +59,13 @@ public class BW {
         logger.info("jvm: {} ({}bit).", System.getProperty("java.version"), System.getProperty("sun.arch.data.model") );
         logger.info("os: {}", System.getProperty("os.name"));
         
-        if ("Windows".equals(System.getProperty("os.name"))) {
+        /* this is pretty hacky but required for now to run BWAPI4J on both Windows and Linux without modifying the source.
+         * 
+         * Possible future solutions:
+         *  - name BWAPI4JBridge and OpenBWAPI4JBridge the same. This way linux loads <name>.so and windows loads <name>.dll
+         *  - build a single bwta.dll rather than libgmp-10 and libmpfr-4 and load bwta.so accordingly on linux.
+         */
+        if (System.getProperty("os.name").contains("Windows")) {
 	        System.loadLibrary("libgmp-10");
 	        System.loadLibrary("libmpfr-4");
 	        System.loadLibrary("BWAPI4JBridge");
