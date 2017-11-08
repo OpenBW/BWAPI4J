37a358ce9c8f54a6f5985a36f737f61742196abb
Adakite Systems <adakitesystems@gmail.com>
2017-11-07 20:04:17 -0600

    BWEM: Test: Fill in data for "BWMapMock" and "MiniTileTest"
    - "BWMock" still needs data.

--- BWAPI4J/src/test/java/bwem/map/MiniTileTest.java
+++ BWAPI4J/src/test/java/bwem/map/MiniTileTest.java
@@ -4,21 +4,24 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openbw.bwapi4j.BW;
+import org.openbw.bwapi4j.WalkPosition;
 
 public class MiniTileTest {
 
-	@BeforeClass 
+	@BeforeClass
     public static void setUpClass() {
-        
+
     }
-    
+
     @Test
     public void heightTest() throws AssertionError {
-    	
+
     	BW bw = new BWMock();
     	Map map = new MapImpl(bw);
     	map.Initialize();
-    	
-    	Assert.assertEquals("mini tile altitude is wrong.", 193, map.GetMiniTile(null).Altitude().intValue());
+
+        Assert.assertEquals("mini tile altitude is wrong.", 243, map.GetMiniTile(new WalkPosition(248, 249)).Altitude().intValue());
+        Assert.assertEquals("mini tile altitude is wrong.", 198, map.GetMiniTile(new WalkPosition(273, 260)).Altitude().intValue());
+        Assert.assertEquals("mini tile altitude is wrong.", 192, map.GetMiniTile(new WalkPosition(273, 261)).Altitude().intValue());
     }
 }
