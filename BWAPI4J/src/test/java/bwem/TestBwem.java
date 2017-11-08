1ca706129b982535657f1753e5ee60433cafb980
Adakite Systems <adakitesystems@gmail.com>
2017-11-07 19:32:25 -0600

    Add file TestBwem.java

---
+++ BWAPI4J/src/test/java/bwem/TestBwem.java
@@ -0,0 +1,178 @@
+package bwem;
+
+import bwem.example.MapPrinterExample;
+import org.openbw.bwapi4j.unit.Unit;
+
+import bwta.BWTA;
+import org.openbw.bwapi4j.BW;
+import org.openbw.bwapi4j.BWEventListener;
+import org.openbw.bwapi4j.Player;
+import org.openbw.bwapi4j.Position;
+
+public class TestBwem implements BWEventListener {
+
+    private BW bw;
+    private BWTA bwta;
+    private BWEM bwem;
+//    private int frame;
+
+    @Override
+    public void onStart() {
+
+        try {
+            System.out.println("onStart");
+            this.bw.getInteractionHandler().enableUserInput();
+
+//            this.bwta = new BWTA();
+//            this.bwta.analyze();
+//            System.out.println("BWTA analysis done.");
+
+            System.out.println("BWEM initialization started.");
+            this.bwem = new BWEM(this.bw);
+            this.bwem.GetMap().Initialize();
+            this.bwem.GetMap().EnableAutomaticPathAnalysis();
+            this.bwem.GetMap().getMapPrinter().Initialize(this.bw, this.bwem.GetMap());
+            MapPrinterExample example = new MapPrinterExample(this.bwem.GetMap().getMapPrinter());
+            example.printMap(this.bwem.GetMap());
+            example.pathExample(this.bwem.GetMap());
+            System.out.println("BWEM initialization completed.");
+
+//            this.frame = 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void onEnd(boolean isWinner) {
+        System.out.println("onEnd: winner: " + isWinner);
+    }
+
+    @Override
+    public void onFrame() {
+
+        try {
+
+//        if (frame == 5) {
+//
+//            System.out.println(this.bwta.getBaseLocations().size() + " base locations found.");
+//            for (BaseLocation base : this.bwta.getBaseLocations()) {
+//
+//                System.out.println("location at " + base.getPosition().getX() + ", " + base.getPosition().getY());
+//            }
+//
+//            System.out.println(this.bwta.getChokepoints().size() + " chokepoints found.");
+//            for (Chokepoint choke : this.bwta.getChokepoints()) {
+//
+//                System.out.println("choke side 1: " + choke.getRegions().first + ", side 2: " + choke.getRegions().second);
+//            }
+//
+//            System.out.println(this.bwta.getRegions().size() + " regions found.");
+//        }
+//
+//        if (bw.getInteractionHandler().isKeyPressed(Key.K_D)) {
+//            System.out.println("D");
+//        }
+//        for (Player player : bw.getAllPlayers()) {
+//            System.out.println("Player " + player.getName() + " has minerals " + player.minerals());
+//        }
+//
+//        this.frame++;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void onSendText(String text) {
+        System.out.println("onSendText: " + text);
+    }
+
+    @Override
+    public void onReceiveText(Player player, String text) {
+        System.out.println("onReceiveText: by " + player + ": " + text);
+    }
+
+    @Override
+    public void onPlayerLeft(Player player) {
+        System.out.println("onPlayerLeft: " + player);
+    }
+
+    @Override
+    public void onNukeDetect(Position target) {
+        System.out.println("onNukeDetect: " + target);
+    }
+
+    @Override
+    public void onUnitDiscover(Unit unit) {
+        System.out.println("onUnitDiscover: " + unit);
+    }
+
+    @Override
+    public void onUnitEvade(Unit unit) {
+        System.out.println("onUnitEvade: " + unit);
+    }
+
+    @Override
+    public void onUnitShow(Unit unit) {
+        System.out.println("onUnitShow: " + unit);
+    }
+
+    @Override
+    public void onUnitHide(Unit unit) {
+        System.out.println("onUnitHide: " + unit);
+    }
+
+    @Override
+    public void onUnitCreate(Unit unit) {
+        System.out.println("onUnitCreate: " + unit);
+    }
+
+    @Override
+    public void onUnitDestroy(Unit unit) {
+        System.out.println("onUnitDestroy: " + unit);
+
+        /* BWEM's unit tracking. */
+        try {
+            this.bwem.GetMap().OnUnitDestroyed(unit);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUnitMorph(Unit unit) {
+        System.out.println("onUnitMorph: " + unit);
+    }
+
+    @Override
+    public void onUnitRenegade(Unit unit) {
+        System.out.println("onUnitRenegade: " + unit);
+    }
+
+    @Override
+    public void onSaveGame(String gameName) {
+        System.out.println("onSaveGame: " + gameName);
+    }
+
+    @Override
+    public void onUnitComplete(Unit unit) {
+        System.out.println("onUnitComplete: " + unit);
+    }
+
+    /**
+     * Test method.
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+
+        TestBwem listener = new TestBwem();
+        BW bw = new BW(listener);
+        listener.bw = bw;
+
+        bw.startGame();
+    }
+}
