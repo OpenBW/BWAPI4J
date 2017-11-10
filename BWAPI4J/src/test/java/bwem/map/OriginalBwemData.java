cbfcc3dfcaebd415b25646458c0c3ae46f37ea0d
Adakite Systems <adakitesystems@gmail.com>
2017-11-09 21:05:18 -0600

    BWEM: Test: all mini tile altitudes
    - Remove "results" arrays. Read full data from file.
    - Add file "OriginalBwemData.java".

--- BWAPI4J/src/test/java/bwem/map/OriginalBwemData.java
+++ BWAPI4J/src/test/java/bwem/map/OriginalBwemData.java
@@ -1,7 +1,6 @@
 package bwem.map;
 
 import org.apache.commons.lang3.mutable.MutableInt;
-import org.openbw.bwapi4j.TilePosition;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,27 +9,20 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class OriginalBwapiData {
+public class OriginalBwemData {
 
-    public final int[] walkabilityInfo_ORIGINAL;
-    public final int[] groundInfo_ORIGINAL;
-    public final int[] buildableInfo_ORIGINAL;
-    public final TilePosition[] startLocations_FightingSpirit_ORIGINAL = {new TilePosition(117, 7), new TilePosition(7, 6), new TilePosition(7, 116), new TilePosition(117, 117)};
+    public final int[] miniTileAltitudes_ORIGINAL;
 
-    public OriginalBwapiData() throws IOException, URISyntaxException {
+    public OriginalBwemData() throws IOException, URISyntaxException {
         int width = 128;
         int height = 128;
         int walkWidth = width * 4;
         int walkHeight = height * 4;
 
-        this.walkabilityInfo_ORIGINAL = new int[walkWidth * walkHeight];
-        this.groundInfo_ORIGINAL = new int[width * height];
-        this.buildableInfo_ORIGINAL = new int[width * height];
+        this.miniTileAltitudes_ORIGINAL = new int[walkWidth * walkHeight];
 
         final String filenameSuffix = "_FightingSpirit" + "_ORIGINAL.txt";
-        populateIntegerArray("walkabilityInfo" + filenameSuffix, this.walkabilityInfo_ORIGINAL, " ");
-        populateIntegerArray("groundInfo" + filenameSuffix, this.groundInfo_ORIGINAL, " ");
-        populateIntegerArray("buildableInfo" + filenameSuffix, this.buildableInfo_ORIGINAL, " ");
+        populateIntegerArray("MiniTile_Altitudes" + filenameSuffix, this.miniTileAltitudes_ORIGINAL, " ");
     }
 
     private void populateIntegerArray(final String filename, final int[] array, final String regex) throws URISyntaxException, IOException {
