c1f2c88a954c973560503d3ef1547c43bb2ae88d
Adakite Systems <adakitesystems@gmail.com>
2017-11-13 14:06:49 -0600

    BWEM: Utils.java: Add method "really_remove_if"

--- BWAPI4J/src/main/java/bwem/util/Utils.java
+++ BWAPI4J/src/main/java/bwem/util/Utils.java
@@ -1,6 +1,9 @@
 package bwem.util;
 
+import bwem.typedef.Pred;
 import org.apache.commons.lang3.mutable.MutableDouble;
+
+import java.util.List;
 
 public final class Utils {
 
@@ -60,6 +63,14 @@ public final class Utils {
     // Returns whether the line segments [a, b] and [c, d] intersect.
     public static boolean intersect(int ax, int ay, int bx, int by, int cx, int cy, int dx, int dy) {
         return get_line_intersection(ax, ay, bx, by, cx, cy, dx, dy, null, null);
+    }
+
+    public static <T> void really_remove_if(final List<T> Container, final Pred pred) {
+        for (int i = 0; i < Container.size(); ++i) {
+            if (pred.isTrue(Container.get(i))) {
+                Container.remove(i--);
+            }
+        }
     }
 
 }
