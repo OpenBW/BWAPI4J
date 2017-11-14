1846c0ba4436c231486920ff16a7d91b21d21484
Adakite Systems <adakitesystems@gmail.com>
2017-11-14 11:54:57 -0600

    BWEM: Bugfix: MiniTilesByDescendingAltitude sorting
    - Remove "DESCENDING" concept from custom comparator and use default
    ascending order with a call to reverse the list after sorting.

--- BWAPI4J/src/main/java/bwem/util/PairGenericAltitudeComparator.java
+++ BWAPI4J/src/main/java/bwem/util/PairGenericAltitudeComparator.java
@@ -7,10 +7,9 @@ import org.apache.commons.lang3.tuple.MutablePair;
 public final class PairGenericAltitudeComparator<T> implements Comparator<MutablePair<T, Altitude>> {
 
     @Override
-    public int compare(MutablePair o1, MutablePair o2) {
-        int a1 = ((Altitude) o1.right).intValue();
-        int a2 = ((Altitude) o2.right).intValue();
-        return (a1 < a2) ? -1 : (a1 > a2) ? 1 : 0;
+    public int compare(MutablePair<T, Altitude> o1, MutablePair<T, Altitude> o2) {
+        int a1 = o1.right.intValue();
+        int a2 = o2.right.intValue();
+        return Integer.compare(a1, a2);
     }
-
 }
