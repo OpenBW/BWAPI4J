cfd6b5d3a90238c1cf40eac17d28d67e98c588b4
IMP <imp@openbw.com>
2017-11-23 17:32:01 -0500

    bugfix: don't treat Critter and special units as player units.

--- BWAPI4J/src/main/java/org/openbw/bwapi4j/unit/SpecialUnit.java
+++ BWAPI4J/src/main/java/org/openbw/bwapi4j/unit/SpecialUnit.java
@@ -2,7 +2,7 @@ package org.openbw.bwapi4j.unit;
 
 import org.openbw.bwapi4j.type.UnitType;
 
-public class SpecialUnit extends PlayerUnit {
+public class SpecialUnit extends Unit {
 
     protected SpecialUnit(int id, UnitType unitType) {
         
