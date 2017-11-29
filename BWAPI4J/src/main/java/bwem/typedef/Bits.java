59ca7d0bf38423ccb74cc926ad7ff922a4141367
Adakite Systems <adakitesystems@gmail.com>
2017-11-29 06:28:19 -0600

    BWEM: Use integer primitive for "Bits.groundHeight"

--- BWAPI4J/src/main/java/bwem/typedef/Bits.java
+++ BWAPI4J/src/main/java/bwem/typedef/Bits.java
@@ -3,12 +3,12 @@ package bwem.typedef;
 public final class Bits {
 
     public byte buildable;
-    public byte groundHeight;
+    public int groundHeight;
     public byte doodad;
 
     public Bits() {
         this.buildable = 0x0;
-        this.groundHeight = 0x0;
+        this.groundHeight = 0;
         this.doodad = 0x0;
     }
 
