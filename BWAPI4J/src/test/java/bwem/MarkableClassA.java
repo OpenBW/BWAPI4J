59d27c7725b1ffaf66d691dc300671bc8de5631f
Adakite Systems <adakitesystems@gmail.com>
2017-12-01 08:14:34 -0600

    BWEM: Add tests for old and new class "Markable"

---
+++ BWAPI4J/src/test/java/bwem/MarkableClassA.java
@@ -0,0 +1,27 @@
+package bwem;
+
+public class MarkableClassA {
+
+    private static final StaticMarkable staticMarkable = new StaticMarkable();
+    private final Markable markable;
+
+    private final int val;
+
+    public MarkableClassA(final int val) {
+        this.markable = new Markable(staticMarkable);
+        this.val = val;
+    }
+
+    public static StaticMarkable getStaticMarkable() {
+        return staticMarkable;
+    }
+
+    public Markable getMarkable() {
+        return this.markable;
+    }
+
+    public int intValue() {
+        return this.val;
+    }
+
+}
---
+++ BWAPI4J/src/test/java/bwem/OldMarkableClassA.java
@@ -0,0 +1,15 @@
+package bwem;
+
+public class OldMarkableClassA extends OldMarkable<OldMarkableClassA> {
+
+    private final int id;
+
+    public OldMarkableClassA(final int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+}
