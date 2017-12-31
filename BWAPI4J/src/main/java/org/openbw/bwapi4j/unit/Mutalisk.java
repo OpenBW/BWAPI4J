ae7f07a15e94443d556a33b89dc69913ccac6064
bytekeeper <therealdante@gmail.com>
2017-12-31 16:58:38 +0100

    Fixes OpenBW/BWAPI4J/#4

--- BWAPI4J/src/main/java/org/openbw/bwapi4j/unit/Mutalisk.java
+++ BWAPI4J/src/main/java/org/openbw/bwapi4j/unit/Mutalisk.java
@@ -6,12 +6,26 @@ import org.openbw.bwapi4j.type.UnitType;
 public class Mutalisk extends MobileUnit implements Organic {
 
     protected Mutalisk(int id) {
-        
+
         super(id, UnitType.Zerg_Mutalisk);
     }
-    
-    public boolean morph() {
-        
+
+    public boolean morph(UnitType type) {
+        if (type == UnitType.Zerg_Guardian) {
+            return morphGuardian();
+        }
+        if (type == UnitType.Zerg_Defiler) {
+            return morphDefiler();
+        }
+        return false;
+    }
+
+    public boolean morphGuardian() {
+
         return issueCommand(this.id, UnitCommandType.Morph.ordinal(), -1, -1, -1, UnitType.Zerg_Guardian.getId());
+    }
+
+    public boolean morphDefiler() {
+        return issueCommand(this.id, UnitCommandType.Morph.ordinal(), -1, -1, -1, UnitType.Zerg_Defiler.getId());
     }
 }
