86d62d4862b63b01a196050265a479cd24ce00df
bytekeeper <therealdante@gmail.com>
2018-01-13 17:14:19 +0100

    Positions outside of the map are not walkable.

--- BWAPI4J/src/main/java/org/openbw/bwapi4j/BWMapImpl.java
+++ BWAPI4J/src/main/java/org/openbw/bwapi4j/BWMapImpl.java
@@ -1,18 +1,18 @@
 package org.openbw.bwapi4j;
 
+import org.openbw.bwapi4j.type.UnitType;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openbw.bwapi4j.type.UnitType;
-
 class BWMapImpl implements BWMap {
 
-	private String mapHash;
+    private String mapHash;
     private String mapFileName;
-    
+
     // Walk resolution
     private int[][] walkabilityInfo;
-    
+
     // Tile resolution
     private int[][] groundInfo;
     private int width;
@@ -20,7 +20,7 @@ class BWMapImpl implements BWMap {
     private ArrayList<TilePosition> startLocations;
 
     BWMapImpl() {
-    	
+
         this.startLocations = new ArrayList<TilePosition>();
     }
 
@@ -29,7 +29,7 @@ class BWMapImpl implements BWMap {
     }
 
     public String mapFileName() {
-    	
+
         return this.mapFileName;
     }
 
@@ -46,7 +46,10 @@ class BWMapImpl implements BWMap {
     }
 
     public boolean isWalkable(int walkX, int walkY) {
-        return this.walkabilityInfo[walkX][walkY] == 1;
+        return walkX >= 0 && walkY >= 0 &&
+                walkX < width * TilePosition.SIZE_IN_PIXELS / WalkPosition.SIZE_IN_PIXELS &&
+                walkY < height * TilePosition.SIZE_IN_PIXELS / WalkPosition.SIZE_IN_PIXELS &&
+                this.walkabilityInfo[walkX][walkY] == 1;
     }
 
     @Override
@@ -63,52 +66,52 @@ class BWMapImpl implements BWMap {
     }
 
     private native int _isBuildable(int tileX, int tileY, boolean considerBuildings);
-    
+
     public boolean isBuildable(int tileX, int tileY, boolean considerBuildings) {
-    	
-    	return _isBuildable(tileX, tileY, considerBuildings) == 1;
+
+        return _isBuildable(tileX, tileY, considerBuildings) == 1;
     }
 
     public boolean isBuildable(TilePosition position, boolean considerBuildings) {
-    	
-    	return _isBuildable(position.getX(), position.getY(), considerBuildings) == 1;
+
+        return _isBuildable(position.getX(), position.getY(), considerBuildings) == 1;
     }
-    
+
     private native int _isExplored(int tileX, int tileY);
-    
+
     public boolean isExplored(int tileX, int tileY) {
-    	
-    	return _isExplored(tileX, tileY) == 1;
+
+        return _isExplored(tileX, tileY) == 1;
     }
-    
+
     public boolean isExplored(TilePosition position) {
-    	
+
         return _isExplored(position.getX(), position.getY()) == 1;
     }
-    
+
     private native int _isVisible(int tileX, int tileY);
-    
+
     public boolean isVisible(int tileX, int tileY) {
-    	
-    	return _isVisible(tileX, tileY) == 1;
+
+        return _isVisible(tileX, tileY) == 1;
     }
-    
+
     public boolean isVisible(TilePosition position) {
-    	
+
         return _isVisible(position.getX(), position.getY()) == 1;
     }
 
     private native int _hasPath(int x1, int y1, int x2, int y2);
 
     public boolean hasPath(Position source, Position destination) {
-    	
+
         return _hasPath(source.getX(), source.getY(), destination.getX(), destination.getY()) == 1;
     }
 
     private native int _canBuildHere(int x, int y, int typeId);
 
     public boolean canBuildHere(TilePosition position, UnitType type) {
-    	
+
         return _canBuildHere(position.getX(), position.getY(), type.getId()) == 1;
     }
 }
