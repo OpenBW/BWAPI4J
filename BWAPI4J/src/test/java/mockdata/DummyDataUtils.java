b720cca81e2c981902f6b2f46a9a1bc6b83983b7
Adakite Systems <adakitesystems@gmail.com>
2018-02-10 18:29:07 -0600

    BWEM: Test: More utils and dummy data

--- BWAPI4J/src/test/java/mockdata/DummyDataUtils.java
+++ BWAPI4J/src/test/java/mockdata/DummyDataUtils.java
@@ -9,6 +9,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public final class DummyDataUtils {
@@ -30,5 +32,26 @@ public final class DummyDataUtils {
         logger.debug("Added " + index + " values");
     }
 
+    public static int[] parseIntegerArray(final String filename, final String regex) throws URISyntaxException, IOException {
+        final MutableInt index = new MutableInt(0);
+        final URI fileURI = DummyDataUtils.class.getResource(filename).toURI();
+        final List<Integer> array = new ArrayList<>();
+        final Stream<String> stream = Files.lines(Paths.get(fileURI));
+        stream.forEach(l -> {
+            for (final String s : l.split(regex)) {
+                array.add(Integer.valueOf(s.trim()));
+            }
+        });
+        stream.close();
+
+        logger.debug("Read " + index + " values");
+
+        final int[] ret = new int[array.size()];
+        for (int i = 0; i < array.size(); ++i) {
+            ret[i] = array.get(i);
+        }
+
+        return ret;
+    }
 
 }
