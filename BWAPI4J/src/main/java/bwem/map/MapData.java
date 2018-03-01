c0c5b1b85f94e078c4f713407dfa1dc7c65cc9a9
Adakite Systems <adakitesystems@gmail.com>
2018-03-01 15:05:31 -0600

    BWEM: Add javadocs to "MapData.java"

--- BWAPI4J/src/main/java/bwem/map/MapData.java
+++ BWAPI4J/src/main/java/bwem/map/MapData.java
@@ -8,35 +8,80 @@ import java.util.List;
 
 public interface MapData {
 
-    // Returns the size of the Map in Tiles.
+    /**
+     * Returns the size of the map in tiles.
+     */
     TilePosition getTileSize();
 
-    // Returns the size of the Map in MiniTiles.
+    /**
+     * Returns the size of the map in walktiles.
+     */
     WalkPosition getWalkSize();
 
-    // Returns the size of the Map in pixels.
+    /**
+     * Returns the size of the map in pixels.
+     */
     Position getPixelSize();
 
-    // Returns the center of the Map in pixels.
+    /**
+     * Returns the center of the map in pixels.
+     */
     Position getCenter();
 
-    // Returns a reference to the starting Locations.
-    // Note: these correspond to BWAPI::getStartLocations().
+    /**
+     * Returns a reference to the starting Locations.<br/>
+     * Note: these correspond to BWAPI::getStartLocations().
+     */
     List<TilePosition> getStartingLocations();
 
+    /**
+     * Tests whether the specified position is inside the map.
+     *
+     * @param tilePosition the specified position
+     */
     boolean isValid(TilePosition tilePosition);
 
+    /**
+     * Tests whether the specified position is inside the map.
+     *
+     * @param walkPosition the specified position
+     */
     boolean isValid(WalkPosition walkPosition);
 
+    /**
+     * Tests whether the specified position is inside the map.
+     *
+     * @param position the specified position
+     */
     boolean isValid(Position position);
 
+    /**
+     * Returns a cropped version of the specified position if
+     * it is not inside the map.
+     *
+     * @param tilePosition the specified position
+     */
     TilePosition crop(TilePosition tilePosition);
 
+    /**
+     * Returns a cropped version of the specified position if
+     * it is not inside the map.
+     *
+     * @param walkPosition the specified position
+     */
     WalkPosition crop(WalkPosition walkPosition);
 
+    /**
+     * Returns a cropped version of the specified position if
+     * it is not inside the map.
+     *
+     * @param position the specified position
+     */
     Position crop(Position position);
 
-    // Returns a random position in the Map in pixels.
+    /**
+     * Returns a random position on the map in pixels.
+     */
     Position getRandomPosition();
 
 }
