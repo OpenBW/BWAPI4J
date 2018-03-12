09e41f27f7999bf9a141a89a2c9cbf380bb74116
Adakite Systems <adakitesystems@gmail.com>
2018-03-12 13:24:38 -0600

    BWEM: Test: Update DataUtils.java

--- BWAPI4J/src/test/java/mockdata/DummyDataUtils.java
+++ BWAPI4J/src/test/java/mockdata/DummyDataUtils.java
@@ -25,19 +25,18 @@ public final class DummyDataUtils {
     private DummyDataUtils() {}
 
     public static int[] populateIntegerArray(final String filename, final String regex) throws IOException {
-        final MutableInt index = new MutableInt(0);
-        InputStream inputStream = DummyDataUtils.class.getResourceAsStream("/mockdata/" + filename + ".tar.bz2");
+        final InputStream inputStream = DummyDataUtils.class.getResourceAsStream("/mockdata/" + filename + ".tar.bz2");
 
-        try (ArchiveInputStream tarIn = new TarArchiveInputStream(new BZip2CompressorInputStream(inputStream));
-             BufferedReader buffer = new BufferedReader(new InputStreamReader(tarIn))) {
-            ArchiveEntry nextEntry = tarIn.getNextEntry();
+        try (final ArchiveInputStream tarIn = new TarArchiveInputStream(new BZip2CompressorInputStream(inputStream));
+            final BufferedReader buffer = new BufferedReader(new InputStreamReader(tarIn))) {
+            final ArchiveEntry nextEntry = tarIn.getNextEntry();
             Assert.assertNotNull(nextEntry);
-            int[] read = buffer.lines()
+            final int[] read = buffer.lines()
                     .flatMap(line -> Stream.of(line.split(regex)))
                     .map(String::trim)
                     .mapToInt(Integer::parseInt)
                     .toArray();
-            logger.debug("Read " + index + " values");
+            logger.debug("Read " + read.length + " values");
             return read;
         }
     }
