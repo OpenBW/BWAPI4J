22f3ddd80f03f176918763e5e83a089ff901dd76
Adakite Systems <adakitesystems@gmail.com>
2018-04-04 12:12:26 -0600

    Merge "BWAPI4JBridge.cpp" into "So.cpp" and clean up

--- BWAPI4JBridge/BWAPI4JBridge/dllmain.cpp
+++ BWAPI4JBridge/BWAPI4JBridge/dllmain.cpp
@@ -1,19 +1,20 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
-#include "stdafx.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
+#include <Windows.h>
+
+#include <BWAPI.h>
+
+extern "C" __declspec(dllexport) void gameInit(BWAPI::Game* game) {
+	BWAPI::BroodwarPtr = game; 
+}
+
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+	switch (ul_reason_for_call) {
+		case DLL_PROCESS_ATTACH:
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
 		break;
 	}
 	return TRUE;
 }
-
