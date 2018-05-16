2d11c1b6235dbec9e805a00b4b411bb309819fcb
Adakite Systems <adakitesystems@gmail.com>
2018-05-16 07:32:33 -0600

    Fix #41: GetEnergy() doesn't work for ComsatStation units

--- BWAPI4J/src/main/java/org/openbw/bwapi4j/unit/ComsatStation.java
+++ BWAPI4J/src/main/java/org/openbw/bwapi4j/unit/ComsatStation.java
@@ -21,8 +21,6 @@
 package org.openbw.bwapi4j.unit;
 
 import org.openbw.bwapi4j.Position;
-import org.openbw.bwapi4j.type.TechType;
-import org.openbw.bwapi4j.type.UnitCommandType;
 import org.openbw.bwapi4j.type.UnitType;
 
 import static org.openbw.bwapi4j.type.TechType.Scanner_Sweep;
@@ -33,19 +31,32 @@ public class ComsatStation extends Addon implements Mechanical, SpellCaster {
     private int energy;
 
     protected ComsatStation(int id, int timeSpotted) {
-        
+
         super(id, UnitType.Terran_Comsat_Station, timeSpotted);
     }
 
+    @Override
+    public void initialize(int[] unitData, int index, int frame) {
+
+        this.energy = 0;
+        super.initialize(unitData, index, frame);
+    }
+
+    @Override
+    public void update(int[] unitData, int index, int frame) {
+
+        this.energy = unitData[index + Unit.ENERGY_INDEX];
+        super.update(unitData, index, frame);
+    }
+
     public boolean scannerSweep(Position p) {
-        
-        return issueCommand(this.id, Use_Tech_Position, -1, p.getX(), p.getY(),
-                Scanner_Sweep.getId());
+
+        return issueCommand(this.id, Use_Tech_Position, -1, p.getX(), p.getY(), Scanner_Sweep.getId());
     }
 
     @Override
     public int getEnergy() {
-        
+
         return this.energy;
     }
 
