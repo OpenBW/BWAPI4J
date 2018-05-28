4a398b1eea90b891d163efc10fe020f80950c838
Adakite Systems <adakitesystems@gmail.com>
2018-05-28 11:57:00 -0600

    Add formatting files

---
+++ BWAPI4JBridge/BWAPI4JBridge/formatting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FORMATTER_BIN=clang-format
+FORMATTER_TARGET_DIR=src/
+
+if [ -x "$(command -v $FORMATTER_BIN)" ]; then
+  echo Running \`$FORMATTER_BIN\' in directory \`$FORMATTER_TARGET_DIR\'
+  find $FORMATTER_TARGET_DIR -type f -exec $FORMATTER_BIN -i {} \;
+else
+  echo error: $FORMATTER_BIN not found. Please run manually or install it on your system. >&2
+  exit 1
+fi
