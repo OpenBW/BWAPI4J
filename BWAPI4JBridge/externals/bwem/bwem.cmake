c7298c24f1210176478cd7cf759778e1f5e021fe
Adakite Systems <adakitesystems@gmail.com>
2018-07-24 16:12:34 -0600

    BWAPI4JBridge: Update CMake scripts for BWEM

--- BWAPI4JBridge/externals/bwem/bwem.cmake
+++ BWAPI4JBridge/externals/bwem/bwem.cmake
@@ -1,3 +1,9 @@
+if (NOT BWEM_CMAKE_INCLUDE)
+set(BWEM_CMAKE_INCLUDE 1)
+
+include(${CMAKE_CURRENT_LIST_DIR}/../../func.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../../bwapi_inc.cmake)
+
 if_unset_then_set(BWEM_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/BWEM-community/BWEM)
 if_unset_then_set(BWEM_INC_DIR ${BWEM_ROOT_DIR}/include)
 if_unset_then_set(BWEM_SRC_DIR ${BWEM_ROOT_DIR}/src)
@@ -9,5 +15,4 @@ add_library(${PROJECT_NAME} STATIC ${BWEM_SRCS})
 
 include_directories(${BWEM_INC_DIR})
 
-if_unset_then_set(BWAPI4JBRIDGE_DIR ${CMAKE_CURRENT_LIST_DIR}/../..)
-include_directories(${BWAPI4JBRIDGE_DIR}/externals/bwapi/bwapi/include)
+endif()
