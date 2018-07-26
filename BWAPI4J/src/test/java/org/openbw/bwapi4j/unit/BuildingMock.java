774092fe3a9e0de1355ad1a3d10804b6f0dffbd5
Adakite Systems <adakitesystems@gmail.com>
2018-07-26 12:31:06 -0600

    Test: Add test for refactoring "Building#getLastKnown..."
    - For reference:
    Refactor "Building#getLastKnown..." methods
    https://github.com/OpenBW/BWAPI4J/issues/49

---
+++ BWAPI4J/src/test/java/org/openbw/bwapi4j/unit/BuildingMock.java
@@ -0,0 +1,16 @@
+package org.openbw.bwapi4j.unit;
+
+import org.openbw.bwapi4j.Position;
+import org.openbw.bwapi4j.TilePosition;
+import org.openbw.bwapi4j.type.UnitType;
+
+public class BuildingMock extends Building {
+    public BuildingMock(int id, UnitType type, int timeSpotted, final TilePosition lastKnownTilePosition, final Position lastKnownPosition) {
+        super(id, type, timeSpotted);
+        super.lastKnownTilePosition = lastKnownTilePosition;
+        super.lastKnownPosition = lastKnownPosition;
+        super.isCompleted = true;
+        super.hitPoints = maxHitPoints();
+        super.shields = maxShields();
+    }
+}
