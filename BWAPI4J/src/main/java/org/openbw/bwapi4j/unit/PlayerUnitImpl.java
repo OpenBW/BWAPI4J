9ca8bcbcd7b5794dbcad77f094f492a661e600cb
Adakite Systems <adakitesystems@gmail.com>
2018-08-22 00:39:11 -0600

    "PlayerUnitImpl" now implements interface "PlayerUnit"

--- BWAPI4J/src/main/java/org/openbw/bwapi4j/unit/PlayerUnitImpl.java
+++ BWAPI4J/src/main/java/org/openbw/bwapi4j/unit/PlayerUnitImpl.java
@@ -34,7 +34,7 @@ import org.openbw.bwapi4j.type.Order;
 import org.openbw.bwapi4j.type.UnitCommandType;
 import org.openbw.bwapi4j.type.UnitType;
 
-public abstract class PlayerUnitImpl extends UnitImpl {
+public abstract class PlayerUnitImpl extends UnitImpl implements PlayerUnit {
   protected PlayerUnitImpl(int id, UnitType unitType) {
     super(id, unitType);
   }
