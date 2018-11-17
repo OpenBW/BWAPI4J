5df857b076b54371977900386f9cc0f94bd75933
Adakite Systems <adakitesystems@gmail.com>
2018-11-17 15:40:42 -0600

    Minor formatting, overrides, TODOs
    - Minor changes to the recent PR.

--- BWAPI4J/src/main/java/org/openbw/bwapi4j/Force.java
+++ BWAPI4J/src/main/java/org/openbw/bwapi4j/Force.java
@@ -3,16 +3,30 @@ package org.openbw.bwapi4j;
 import java.util.List;
 
 public class Force {
+  public int getID() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public int getID() {
-        throw new UnsupportedOperationException("TODO");    // TODO
-    }
+  public String getName() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public String getName() {
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public List<Player> getPlayers() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public List<Player> getPlayers() {
-        throw new UnsupportedOperationException("TODO"); // TODO
+  @Override
+  public boolean equals(final Object object) {
+    if (object instanceof Force) {
+      final Force that = (Force) object;
+      return this.getID() == that.getID();
+    } else {
+      return false;
     }
+  }
+
+  @Override
+  public int hashCode() {
+    return getID();
+  }
 }
--- BWAPI4J/src/main/java/org/openbw/bwapi4j/UnitCommand.java
+++ BWAPI4J/src/main/java/org/openbw/bwapi4j/UnitCommand.java
@@ -8,269 +8,299 @@ import org.openbw.bwapi4j.unit.Unit;
 
 // TODO
 public class UnitCommand {
+  private Unit unit = null;
+  private UnitCommandType unitCommandType = UnitCommandType.None;
+  private Unit target = null;
+  private int x;
+  private int y;
+  private int extra;
 
-    private Unit unit = null;
+  public UnitCommand() {}
 
-    private UnitCommandType unitCommandType = UnitCommandType.None;
+  public UnitCommand(
+      Unit unit, UnitCommandType unitCommandType, Unit target, int x, int y, int extra) {
+    this.unit = unit;
+    this.unitCommandType = unitCommandType;
+    this.target = target;
+    this.x = x;
+    this.y = y;
+    this.extra = extra;
+  }
 
-    private Unit target = null;
+  public UnitCommand(Unit unit, UnitCommandType type) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    private int x, y;
+  public static UnitCommand attack(Unit unit, Unit target) {
+    return attack(unit, target, false);
+  }
 
-    private int extra;
+  public static UnitCommand attack(Unit unit, Unit target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public UnitCommand(){
+  public static UnitCommand build(Unit unit, TilePosition target, UnitType type) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    }
+  public static UnitCommand buildAddon(Unit unit, UnitType type) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public UnitCommand(Unit unit, UnitCommandType unitCommandType, Unit target, int x, int y, int extra) {
-        this.unit=unit;
-        this.unitCommandType=unitCommandType;
-        this.target=target;
-        this.x=x;
-        this.y=y;
-        this.extra=extra;
-    }
+  public static UnitCommand train(Unit unit, UnitType type) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public UnitCommand(Unit unit, UnitCommandType type) {
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand morph(Unit unit, UnitType type) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand attack(Unit unit, Unit target) {
-        return attack(unit, target, false);
-    }
+  public static UnitCommand research(Unit unit, TechType tech) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand attack(Unit unit, Unit target, boolean shiftQueueCommand) {
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand upgrade(Unit unit, UpgradeType upgrade) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand build(Unit unit, TilePosition target, UnitType type){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand setRallyPoint(Unit unit, Position target) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand buildAddon(Unit unit, UnitType type) {
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand setRallyPoint(Unit unit, Unit target) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand train(Unit unit, UnitType type){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand move(Unit unit, Position target) {
+    return move(unit, target, false);
+  }
 
-    public static UnitCommand morph(Unit unit, UnitType type){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand research(Unit unit, TechType tech){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand upgrade(Unit unit, UpgradeType upgrade){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand setRallyPoint(Unit unit, Position target){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand move(Unit unit, Position target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand setRallyPoint(Unit unit, Unit target){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand patrol(Unit unit, Position target) {
+    return patrol(unit, target, false);
+  }
 
-    public static UnitCommand move(Unit unit, Position target){
-        return move(unit, target, false);
-    }
+  public static UnitCommand patrol(Unit unit, Position target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand move(Unit unit, Position target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand holdPosition(Unit unit) {
+    return holdPosition(unit, false);
+  }
 
-    public static UnitCommand patrol(Unit unit, Position target){
-        return patrol(unit, target, false);
-    }
+  public static UnitCommand holdPosition(Unit unit, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand patrol(Unit unit, Position target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand stop(Unit unit) {
+    return stop(unit, false);
+  }
 
-    public static UnitCommand holdPosition(Unit unit){
-        return holdPosition(unit, false);
-    }
+  public static UnitCommand stop(Unit unit, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand holdPosition(Unit unit, boolean shiftQueueCommand) {
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand follow(Unit unit, Unit target) {
+    return follow(unit, target, false);
+  }
 
-    public static UnitCommand stop(Unit unit){
-        return stop(unit,false);
-    }
+  public static UnitCommand follow(Unit unit, Unit target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand stop(Unit unit, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand gather(Unit unit, Unit target) {
+    return gather(unit, target, false);
+  }
 
-    public static UnitCommand follow(Unit unit, Unit target){
-        return follow(unit, target, false);
-    }
+  public static UnitCommand gather(Unit unit, Unit target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand follow(Unit unit, Unit target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand gather(Unit unit, Unit target){
-        return gather(unit, target, false);
-    }
-    public static UnitCommand gather(Unit unit, Unit target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand returnCargo(Unit unit){
-        return returnCargo(unit, false);
-    }
-    public static UnitCommand returnCargo(Unit unit, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand returnCargo(Unit unit) {
+    return returnCargo(unit, false);
+  }
 
-    public static UnitCommand repair(Unit unit, Unit target){
-        return repair(unit, target, false);
-    }
+  public static UnitCommand returnCargo(Unit unit, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand repair(Unit unit, Unit target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand burrow(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand unburrow(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand cloak(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand decloak(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand siege(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand unsiege(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand lift(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand land(Unit unit, TilePosition target){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand load(Unit unit, Unit target){
-        return load(unit, target, false);
-    }
+  public static UnitCommand repair(Unit unit, Unit target) {
+    return repair(unit, target, false);
+  }
 
-    public static UnitCommand load(Unit unit, Unit target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand unload(Unit unit, Unit target){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand repair(Unit unit, Unit target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand unloadAll(Unit unit){
-        return unloadAll(unit, false);
-    }
-    public static UnitCommand unloadAll(Unit unit, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand unloadAll(Unit unit, Position target){
-        return unloadAll(unit, target, false);
-    }
+  public static UnitCommand burrow(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand unloadAll(Unit unit, Position target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand unburrow(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand rightClick(Unit unit, Position target){
-        return rightClick(unit, target, false);
-    }
+  public static UnitCommand cloak(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand rightClick(Unit unit, Position target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand decloak(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public static UnitCommand rightClick(Unit unit, Unit target){
-        return rightClick(unit, target, false);
-    }
-    public static UnitCommand rightClick(Unit unit, Unit target, boolean shiftQueueCommand){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand haltConstruction(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand cancelConstruction(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand cancelAddon(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand cancelTrain(Unit unit){
-        return cancelTrain(unit, -2);
-    }
-    public static UnitCommand cancelTrain(Unit unit, int slot){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand cancelMorph(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand cancelResearch(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand cancelUpgrade(Unit unit){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand useTech(Unit unit,TechType tech){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand useTech(Unit unit, TechType tech, Position target){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand useTech(Unit unit, TechType tech, Unit target){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
-    public static UnitCommand placeCOP(Unit unit, TilePosition target){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand siege(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public final UnitCommandType getType() {
-        return unitCommandType;
-    }
-    public Unit getUnit() {
-        return unit;
-    }
+  public static UnitCommand unsiege(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public Unit getTarget(){
-        return target;
-    }
+  public static UnitCommand lift(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public Position getTargetPosition(){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand land(Unit unit, TilePosition target) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public TilePosition getTargetTilePosition(){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand load(Unit unit, Unit target) {
+    return load(unit, target, false);
+  }
 
-    public UnitType getUnitType(){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand load(Unit unit, Unit target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public TechType getTechType(){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand unload(Unit unit, Unit target) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public UpgradeType getUpgradeType(){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand unloadAll(Unit unit) {
+    return unloadAll(unit, false);
+  }
 
-    public int getSlot(){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand unloadAll(Unit unit, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
 
-    public boolean isQueued(){
-        throw new UnsupportedOperationException("TODO"); // TODO
-    }
+  public static UnitCommand unloadAll(Unit unit, Position target) {
+    return unloadAll(unit, target, false);
+  }
 
-    //To be implemented, when there will be an abstract point class
-    //void assignTarget(Point<T, S> target)
+  public static UnitCommand unloadAll(Unit unit, Position target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand rightClick(Unit unit, Position target) {
+    return rightClick(unit, target, false);
+  }
+
+  public static UnitCommand rightClick(Unit unit, Position target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand rightClick(Unit unit, Unit target) {
+    return rightClick(unit, target, false);
+  }
+
+  public static UnitCommand rightClick(Unit unit, Unit target, boolean shiftQueueCommand) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand haltConstruction(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand cancelConstruction(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand cancelAddon(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand cancelTrain(Unit unit) {
+    return cancelTrain(unit, -2);
+  }
+
+  public static UnitCommand cancelTrain(Unit unit, int slot) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand cancelMorph(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand cancelResearch(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand cancelUpgrade(Unit unit) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand useTech(Unit unit, TechType tech) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand useTech(Unit unit, TechType tech, Position target) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand useTech(Unit unit, TechType tech, Unit target) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public static UnitCommand placeCOP(Unit unit, TilePosition target) {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public final UnitCommandType getType() {
+    return unitCommandType;
+  }
+
+  public Unit getUnit() {
+    return unit;
+  }
+
+  public Unit getTarget() {
+    return target;
+  }
+
+  public Position getTargetPosition() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public TilePosition getTargetTilePosition() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public UnitType getUnitType() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public TechType getTechType() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public UpgradeType getUpgradeType() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public int getSlot() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  public boolean isQueued() {
+    throw new UnsupportedOperationException("TODO"); // TODO
+  }
+
+  // To be implemented, when there will be an abstract point class
+  // void assignTarget(Point<T, S> target)
+
+  // TODO: equals and hashCode overrides
 }
